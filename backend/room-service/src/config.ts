e58491da633eacdb17b06052448410fc36426c45
Mert Korur <mrtkorur@gmail.com>
2025-11-11 01:47:15 +0100

    feat: add WebSocket support by including 'ws' dependency and configuring WS_PORT

--- backend/room-service/src/config.ts
+++ backend/room-service/src/config.ts
@@ -2,5 +2,6 @@ export const CONFIG = {
   PORT: process.env.PORT || 3002,
   SERVICE_NAME: "Room Service",
   USER_SERVICE_URL: process.env.USER_SERVICE_URL || "http://localhost:3001",
-  GAME_RULES_SERVICE_URL: process.env.GAME_RULES_SERVICE_URL || "http://localhost:3003"
+  GAME_RULES_SERVICE_URL: process.env.GAME_RULES_SERVICE_URL || "http://localhost:3003",
+  WS_PORT: process.env.WS_PORT
 };
